32f10097d48f9db77e6a73a83a63eea14e9a5170
Stanislav Podmazov <stanislav.podmazov@microsoft.wsei.edu.pl>
2022-03-21 01:58:28 +0100

    1

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -9,15 +9,24 @@ using Newtonsoft.Json.Linq;
 
 namespace GradeBook.GradeBooks
 {
+
     public class BaseGradeBook
     {
+
+        public GradeBookType Type { get; set; }
+
         public string Name { get; set; }
         public List<Student> Students { get; set; }
+
+        public BaseGradeBook(string name, bool isWeighted)
+        {
+            Name = name;
+            Students = new List<Student>();
+        }
 
         public BaseGradeBook(string name)
         {
             Name = name;
-            Students = new List<Student>();
         }
 
         public void AddStudent(Student student)
